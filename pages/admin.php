d1b10f689841a2653d1d332c6bd6ff200c1bb3ab
lauri23106 <146861978+lauri23106@users.noreply.github.com>
2024-02-09 19:54:29 +0200

    Update admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -23,6 +23,9 @@
    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
    <link rel="stylesheet" href="./styles/index.css">
+   
+   <!-- Link to admin CSS -->
+   <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
    
@@ -82,3 +85,4 @@
 
 </body>
 </html>
+
