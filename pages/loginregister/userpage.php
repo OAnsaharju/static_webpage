36c9895c495e16c70bfbf7ed5d76a7eaed56203a
aku23004 <aku23004@student.hamk.fi>
2024-02-13 17:41:19 +0200

    Update

--- pages/loginregister/userpage.php
+++ pages/loginregister/userpage.php
@@ -2,8 +2,6 @@
 
 @include 'config.php';
 
-
-
 ?>
 
 <!DOCTYPE html>
@@ -20,43 +18,120 @@
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
-   <link rel="stylesheet" href="./user.css">
+   <link rel="stylesheet" href="../../styles/user.css">
 </head>
 <body>
    
 <div class="container">
 
-   <!-- Header from index.php -->
-   <nav class="navbar navbar-expand-lg">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg">
       <a href="#" class="navbar-brand mb-0 h1">
-         <span class="logo">
-            <img class="d-inline-block align-top" src="../../images/websitelogo.png" alt="Logo">
-         </span>
-         <span class="title">Website</span>
+        <span class="logo">
+          <img
+            class="d-inline-block align-top"
+            src="./images/websitelogo.png"
+            alt="Logo"
+            
+          />
+        </span>
+        <span class="title">Website</span>
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbarfont">Menu</span>
-         <span class="navbar-toggler-icon"></span>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbarfont">Menu</span>
+        <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-         <ul class="navbar-nav">
-            <li class="nav-item">
-               <a class="nav-link active" aria-current="page" href="#"><span class="navbarfont">Home</span></a>
+
+      <div
+        class="collapse navbar-collapse justify-content-center"
+        id="navbarNav"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../#">
+              <span class="navbarfont">Home</span>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link active" href="../about.php">
+              <span class="navbarfont">About</span>
+            </a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link active dropdown-toggle"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <span class="navbarfont">Customer service</span>
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <a class="dropdown-item" href="../contact.php"
+                  >Contact us</a
+                >
+              </li>
+              <li><a class="dropdown-item" href="../FAQ.php">FAQ</a></li>
+            </ul>
+          </li>
+          
+          <?php
+          session_start();
+          if(isset($_SESSION['admin'])){
+            ?>
+            <li class = "nav-item">
+              <a class = "nav-link active" href = "./adminpage.php">
+                <span class = "navbarfontlogin">Admin page</span>
+              </a>
+              <a class = "nav-link active" href = "./logout.php">
+                <span class = "navbarfontlogin">Logout</span>
+              </a>
             </li>
-            <li class="nav-item">
-               <a class="nav-link active" href="pages/about.html"><span class="navbarfont">About</span></a>
+      <?php
+          }
+          elseif(isset($_SESSION['user'])){
+            ?>
+            <li class = "nav-item">
+              <a class = "nav-link active" href = "./userpage.php">
+                <span class = "navbarfontlogin">User page</span>
+              </a>
+              <a class = "nav-link active" href = "./logout.php">
+                <span class = "navbarfontlogin">Logout</span>
+              </a>
             </li>
-            <li class="nav-item dropdown">
-               <a class="nav-link active dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"><span class="navbarfont">Customer service</span></a>
-               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="pages/contact.html">Contact us</a></li>
-                  <li><a class="dropdown-item" href="pages/FAQ.html">FAQ</a></li>
-               </ul>
-            </li>
-         </ul>
+              <?php
+          } else {
+                ?>
+                <li class = "nav-item">
+          <a class = "nav-link active" href = "./login.php">
+            <span class = "navbarfontlogin">Login</span>
+          </a>
+        
+          <a class = "nav-link active" href = "./register.php">
+            <span class = "navbarfontlogin">Register</span>
+          </a>
+        </li>
+              </ul>
+        <?php
+      }
+              ?>
       </div>
-   </nav>
-   <!-- End of Navbar -->
+    </nav>
+    <!-- End of Navbar -->
+
+
 <div class='usertable'>
    <div class='usertable-header'>
   <h2>Users</h2>
